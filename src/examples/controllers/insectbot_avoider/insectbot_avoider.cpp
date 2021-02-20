f1316cff8e3d123c7aa5bacbc266b85654abc0bb
supgilad <supgilad@gitbug>
2021-02-20 21:39:14 +0200

    insectbot stops and unstops

--- src/examples/controllers/insectbot_avoider/insectbot_avoider.cpp
+++ src/examples/controllers/insectbot_avoider/insectbot_avoider.cpp
@@ -10,29 +10,31 @@
 /****************************************/
 /****************************************/
 
-#define PIN_FORWARD 1.5f;
-#define PIN_TURN    1.87f;
-#define PIN_STOP    0.0f;
+#define PIN_FORWARD 2.0f;
+#define PIN_TURN 1.87f;
+#define PIN_STOP 0.0f;
+#define STOP_PROB 10;
+#define MOVE_PROB 5;
 
-CInsectbotAvoider::CInsectbotAvoider() :
-   m_pcMotors(NULL),
-   m_sensor(NULL),
-   m_tCurrentState(KILOBOT_STATE_STOP),
-   m_tPreviousState(KILOBOT_STATE_STOP),
-   m_unMaxMotionSteps(1510),
-   m_unCountMotionSteps(0),
-   m_unMaxTurningSteps(90), // = pi/(omega delta_t) = pi/(v*delta_t/l) = (pi*l)/(v*delta_t)
-   m_unCountTurningSteps(130),
-   m_fMotorL(0.0f),
-   m_fMotorR(0.0f)
+CInsectbotAvoider::CInsectbotAvoider() : m_pcMotors(NULL),
+                                         m_sensor(NULL),
+                                         m_tCurrentState(KILOBOT_STATE_STOP),
+                                         m_tPreviousState(KILOBOT_STATE_STOP),
+                                         m_unMaxMotionSteps(1510),
+                                         m_unCountMotionSteps(0),
+                                         m_unMaxTurningSteps(90), // = pi/(omega delta_t) = pi/(v*delta_t/l) = (pi*l)/(v*delta_t)
+                                         m_unCountTurningSteps(130),
+                                         m_fMotorL(0.0f),
+                                         m_fMotorR(0.0f)
 {
-   m_pcRNG = CRandom::CreateRNG( "argos" );
+   m_pcRNG = CRandom::CreateRNG("argos");
 }
 
 /****************************************/
 /****************************************/
 
-void CInsectbotAvoider::Init(TConfigurationNode& t_node) {
+void CInsectbotAvoider::Init(TConfigurationNode &t_node)
+{
    /*
     * Get sensor/actuator handles
     *
@@ -56,12 +58,13 @@ void CInsectbotAvoider::Init(TConfigurationNode& t_node) {
     * occurs.
     */
    // Get sensor/actuator handles
-   m_pcMotors    = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
-   m_sensor    = GetSensor<CCI_ProximitySensor>("proximity");
+   m_pcMotors = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
+   m_sensor = GetSensor<CCI_ProximitySensor>("proximity");
 
    // Parse the configuration file
-   GetNodeAttributeOrDefault(t_node, "max_motion_steps", m_unMaxMotionSteps, m_unMaxMotionSteps );
-   if( m_unMaxMotionSteps == 0 ) {
+   GetNodeAttributeOrDefault(t_node, "max_motion_steps", m_unMaxMotionSteps, m_unMaxMotionSteps);
+   if (m_unMaxMotionSteps == 0)
+   {
       LOGERR << "[FATAL] Invalid value for num_moving_steps (" << m_unMaxMotionSteps << "). Should be a positive integer." << std::endl;
    }
 
@@ -71,9 +74,10 @@ void CInsectbotAvoider::Init(TConfigurationNode& t_node) {
 /****************************************/
 /****************************************/
 
-void CInsectbotAvoider::Reset() {
+void CInsectbotAvoider::Reset()
+{
    // reset/intialise the robot state
-   m_unCountMotionSteps = m_pcRNG->Uniform(CRange<UInt32>(1,m_unMaxMotionSteps+1));
+   m_unCountMotionSteps = m_pcRNG->Uniform(CRange<UInt32>(1, m_unMaxMotionSteps + 1));
    m_tCurrentState = KILOBOT_STATE_MOVING;
    m_tPreviousState = KILOBOT_STATE_MOVING;
    m_fMotorL = m_fMotorR = PIN_FORWARD;
@@ -82,86 +86,86 @@ void CInsectbotAvoider::Reset() {
 /****************************************/
 /****************************************/
 
-void CInsectbotAvoider::ControlStep() {
+static bool isReadingInRange(double reading, double min, double max)
+{
+   return (reading > min && reading < max);
+}
+
+void CInsectbotAvoider::ControlStep()
+{
    // compute the robot motion: move forward for a fixed amount of
    // time, and rotate cw/ccw for a random amount of time
    // max rotation: 180 degrees as determined by m_unMaxTurningSteps
    m_tPreviousState = m_tCurrentState;
-   const std::vector<double>& tProxReads = m_sensor->GetReadings();
+   const std::vector<double> &tProxReads = m_sensor->GetReadings();
    // /* Sum them together */
    // double avarageDistFromObject;
    // for(size_t i = 0; i < tProxReads.size(); ++i) {
-      // std::cout<< "reading "<<i<<" : "<< tProxReads[i]<<"\n";
-      // if(tProxReads[i]<=0.0000000001){
-         // tProxReads[i]=111111;
-      // }
-      // avarageDistFromObject += tProxReads[i];
+   // std::cout<< "reading "<<i<<" : "<< tProxReads[i]<<"\n";
+   // if(tProxReads[i]<=0.0000000001){
+   // tProxReads[i]=111111;
+   // }
+   // avarageDistFromObject += tProxReads[i];
    // }
    // avarageDistFromObject /= tProxReads.size();
-   double frontDistFromObject = std::min(tProxReads[0],tProxReads[23]);
-      double frontEdgesDistFromObject = std::min(tProxReads[1],tProxReads[22]);
-   double leftDistFromObject = std::min(tProxReads[5],tProxReads[6]);
-   double rightDistFromObject = std::min(tProxReads[18],tProxReads[17]);
+   double frontDistFromObject = std::min(tProxReads[0], tProxReads[23]);
+   double frontEdgesDistFromObject = std::min(tProxReads[1], tProxReads[22]);
+   double leftDistFromObject = std::min(tProxReads[5], tProxReads[6]);
+   double rightDistFromObject = std::min(tProxReads[18], tProxReads[17]);
    // /* If the angle of the vector is small enough and the closest obstacle
    //  * is far enough, continue going straight, otherwise curve a little
    //  */
    // CRadians cAngle = cAccumulator.Angle();
    // if(m_cGoStraightAngleRange.WithinMinBoundIncludedMaxBoundIncluded(cAngle) &&
-double dist = 2.0f;
+   double dist = 3.0f;
 
+   // if (m_tCurrentState == KILOBOT_STATE_TURNING)
+   // {
+   // if (!(frontDistFromObject > 0 && frontDistFromObject < dist) && !(frontEdgesDistFromObject > 0 && frontEdgesDistFromObject < dist) && --m_unCountTurningSteps == 0)
+   // {
+   // m_fMotorL = m_fMotorR = PIN_FORWARD;
+   // m_unCountMotionSteps = m_unMaxMotionSteps;
+   // m_tCurrentState = KILOBOT_STATE_MOVING;
+   // }
+   // }
+   if (m_tCurrentState == KILOBOT_STATE_MOVING)
+   {
+      UInt32 shouldContinue = m_pcRNG->Uniform(CRange<UInt32>(0, 100));
+      if (!shouldContinue)
+      {
+         m_fMotorL = m_fMotorR = PIN_STOP;
+         m_tCurrentState = KILOBOT_STATE_STOP;
+         return;
+      }
 
-   switch(m_tCurrentState) {
-   case KILOBOT_STATE_TURNING:
-      if( !(frontDistFromObject>0 && frontDistFromObject < dist)&&!(frontEdgesDistFromObject >0 && frontEdgesDistFromObject<dist)&& --m_unCountTurningSteps == 0 ) {
+      if (isReadingInRange(frontDistFromObject,0,dist) || isReadingInRange(frontEdgesDistFromObject,0,dist))
+      {
+         m_fMotorL = PIN_TURN;
+         m_fMotorR = PIN_STOP;         // m_unCountTurningSteps = m_pcRNG->Uniform(CRange<UInt32>(m_unMaxTurningSteps - 40, m_unMaxTurningSteps));
+      }
+      else if (isReadingInRange(leftDistFromObject,0,dist))
+      {
+         m_fMotorL = PIN_TURN;
+         m_fMotorR = PIN_STOP;
+      }
+      else if (isReadingInRange(rightDistFromObject,0,dist))
+      {
+         m_fMotorL = PIN_STOP;
+         m_fMotorR = PIN_TURN;
+      }
+   }
+   else if (m_tCurrentState == KILOBOT_STATE_STOP)
+   {
+      UInt32 shouldStayStopped = m_pcRNG->Uniform(CRange<UInt32>(0, 50));
+      if (!shouldStayStopped)
+      {
          m_fMotorL = m_fMotorR = PIN_FORWARD;
-         m_unCountMotionSteps = m_unMaxMotionSteps;
          m_tCurrentState = KILOBOT_STATE_MOVING;
       }
-      break;
-
-   case KILOBOT_STATE_MOVING:
-      // std::cout<< "sensor 1 "<< reads[0]<<"\n";
-         // std::cout<< "front dist : "<< frontDistFromObject<<"\n";
-      
-       if(  (frontDistFromObject>0 && frontDistFromObject < dist)||(frontEdgesDistFromObject >0 && frontEdgesDistFromObject<dist)) {
-            m_fMotorL = PIN_TURN;
-            m_fMotorR = PIN_STOP;
-         m_unCountTurningSteps = m_pcRNG->Uniform(CRange<UInt32>(m_unMaxTurningSteps-40,m_unMaxTurningSteps));
-         m_tCurrentState = KILOBOT_STATE_TURNING;
+      else
+      {
+         m_fMotorL = m_fMotorR = PIN_STOP;
       }
-      else if(  (leftDistFromObject>0 && leftDistFromObject < dist)) {
-            m_fMotorL = PIN_TURN;
-            m_fMotorR = PIN_STOP;
-            m_unCountTurningSteps = m_pcRNG->Uniform(CRange<UInt32>(m_unMaxTurningSteps-40,m_unMaxTurningSteps));
-            m_tCurrentState = KILOBOT_STATE_TURNING;
-      } else if (  (rightDistFromObject>0 && rightDistFromObject < dist)){
-            m_fMotorL = PIN_STOP;
-            m_fMotorR = PIN_TURN;
-            m_unCountTurningSteps = m_pcRNG->Uniform(CRange<UInt32>(m_unMaxTurningSteps-40,m_unMaxTurningSteps));
-         m_tCurrentState = KILOBOT_STATE_TURNING;
-      }
-      // else{
-      //    m_unCountMotionSteps--;
-      //    if (m_unCountMotionSteps==0){
-      //    UInt32 direction = m_pcRNG->Uniform(CRange<UInt32>(0,2));
-      //    if( direction == 0 ) {
-      //       m_fMotorL = PIN_TURN;
-      //       m_fMotorR = PIN_STOP;
-      //    }
-      //    else {
-      //       m_fMotorL = PIN_STOP;
-      //       m_fMotorR = PIN_TURN;
-      //    }
-      //    m_unCountTurningSteps = m_pcRNG->Uniform(CRange<UInt32>(m_unMaxTurningSteps-40,m_unMaxTurningSteps));
-      //    m_tCurrentState = KILOBOT_STATE_TURNING;
-      //    }
-      // }
-      break;
-
-   case KILOBOT_STATE_STOP:
-   default:
-      m_fMotorL = m_fMotorR = PIN_STOP;
-      break;
    };
 
    m_pcMotors->SetLinearVelocity(m_fMotorL, m_fMotorR);
@@ -169,7 +173,6 @@ double dist = 2.0f;
 
 /****************************************/
 /****************************************/
-
 
 /*
  * This statement notifies ARGoS of the existence of the controller.
