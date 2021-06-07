293ba3605856a4fbe11f9a1e060ccefc686d7bd3
supgilad <supgilad@gitbug>
2021-06-07 23:50:06 +0300

    refactored python script

--- src/scripts/generate_insectbot_torus_experiment.py
+++ src/scripts/generate_insectbot_torus_experiment.py
@@ -29,33 +29,40 @@ def build_cylinder_xml(radius):
 def build_robots_xml(radius,num_robots,controller_id):
     ''' 
         You can distribute entities randomly. Here, we distribute
-        10 insectbots in this way:
+        num_robots insectbots in this way:
         - the position is uniformly distributed
-        on the ground, in the square whose corners are (-2,-2) and (2,2)
-        - the orientations are non-zero only when rotating around Z and chosen
-        from a gaussian distribution, whose mean is zero degrees and
-        standard deviation is 360 degrees.
+        on the ground, in the square whose corners are (-radius,-radius) and (radius,radius)
         distribute options - uniform, gaussian,constant, grid
     '''
-    robot_area=radius/1.5
+    robot_distribute_area=radius/1.5 # we want the robots to be inside the circle so made it smaller
     return '<distribute>\
     <position method="uniform"\
-     min="'+str(-robot_area)+','+str(-robot_area)+',0"\
-     max="'+str(robot_area)+','+str(robot_area)+',0" />\
+     min="'+str(-robot_distribute_area)+','+str(-robot_distribute_area)+',0"\
+     max="'+str(robot_distribute_area)+','+str(robot_distribute_area)+',0" />\
       <orientation method="gaussian" mean="0,0,0" std_dev="360,0,0" />\
       <entity quantity="'+str(num_robots)+'" max_trials="100"><insectbot id="kb-'+str(controller_id)+'-">\
           <controller config="'+str(controller_id)+'" /></insectbot></entity>\
     </distribute>'
-
-def main():
-    radius, cylinder_radius, num_robots1,num_robots2 = float(sys.argv[1]),float(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4])
+def build_circle_xml(radius):
     all_points_of_circle = get_all_points_of_circle(radius=radius)
     boxes_as_points_of_a_circle = (build_box(i,point['x'],point['y']) for i,point in enumerate(all_points_of_circle))
     circle_xml = "".join(list(boxes_as_points_of_a_circle))
-
+    return circle_xml
+def main():
+    '''
+    Since Argos have no circle arena option this script creates a circle out of boxes!
+    We create a circle XML which is made from a collection of boxes.
+    We also create a cylinder XML to be put inside the circle to make it a torus.
+    We also want to create an XML which distributes robots inside the torus.
+    The XMLs for the circle,cylinder,robots are injected to the experiment file, by replacing
+    placeholders, if exists.
+    '''
+    circle_radius, cylinder_radius, num_robots1,num_robots2 = float(sys.argv[1]),float(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4])
+    
+    circle_xml = build_circle_xml(radius=circle_radius)
     cilynder_xml = build_cylinder_xml(radius=cylinder_radius)
-    robots1_xml= build_robots_xml(radius=radius,num_robots=num_robots1,controller_id=1)
-    robots2_xml= build_robots_xml(radius=radius,num_robots=num_robots2,controller_id=2)
+    robots1_xml= build_robots_xml(radius=circle_radius,num_robots=num_robots1,controller_id=1)
+    robots2_xml= build_robots_xml(radius=circle_radius,num_robots=num_robots2,controller_id=2)
 
     with open(sys.argv[5], "rt") as fin:
         with open(sys.argv[6], "wt") as fout:
